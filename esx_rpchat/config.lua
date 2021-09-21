0d2ff194e9a3196510e215443000d7431f87e785
GXRP.eu <84440704+Charlee22@users.noreply.github.com>
2021-09-21 19:54:45 +0200

    Add files via upload

---
+++ esx_rpchat/config.lua
@@ -0,0 +1,25 @@
+Config        = {}
+Config.Locale = 'en'
+
+Config.EnableESXIdentity = true -- only turn this on if you are using esx_identity and want to use RP names
+Config.OnlyFirstname     = false
+
+Config.usingIdentity = true
+
+
+
+
+Config.AnnounceMinutes = 3  --minutes 
+
+Config.Messages = {
+	{message = "Vítej na našem serveru! Jsme rádi, že u nás máš zájem erpit! Pokud s něčím potřebuješ pomoct, napiš si report. Jsme tu pro vás!"},
+}
+
+Config.AnnounceMinutes2 = 5  --minutes 
+
+Config.Messages2 = {
+	{message = "Náš discord https://discord.gg/K2EvBW6gdG"},
+}
+
+
+
